2a8003b5959210efabdcedad6a343f9246736660
JulKhizhn <127053070+JulKhizhn@users.noreply.github.com>
2024-12-16 14:23:44 +0300

    Доработка методов GET, POST, описание

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
-const WebSocket = require('ws'); // Подключаем WebSocket
-const multer = require('multer'); // Для загрузки файлов
+const WebSocket = require('ws'); // Подключаем WebSocket.
+const multer = require('multer'); // Для загрузки файлов.
 const path = require('path');
 
 const app = express();
@@ -20,54 +20,54 @@ const pool = new Pool({
     port: 5432,
 });
 
-// Переменная для хранения ID текущей станции
+// Переменная для хранения ID текущей станции.
 let currentStationId = 1;
 
-// Создаём WebSocket-сервер
+// Создание WebSocket-сервера.
 const wss = new WebSocket.Server({ noServer: true });
 
-// Храним подключённые клиенты
+// Хранение подключенных клиентов.
 const clients = new Set();
 
-// Когда подключается новый клиент
+// Подключение нового клиента.
 wss.on('connection', (ws) => {
     clients.add(ws);
 
-    // Удаляем клиента при отключении
+    // Удаление клиента при отключении.
     ws.on('close', () => {
         clients.delete(ws);
     });
 });
 
-// Широковещательная отправка данных всем клиентам
-// Функция для трансляции сообщения всем клиентам
+// Широковещательная отправка данных всем клиентам.
+// Функция для трансляции сообщения всем клиентам.
 function broadcast(type, data) {
     const message = JSON.stringify({
-        type: type,  // Тип сообщения
-        data: data   // Данные для отправки
+        type: type,
+        data: data
     });
 
-    // Отправляем сообщение всем подключенным клиентам
+    // Отправка сообщения всем подключенным клиентам.
     clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(message);
         }
     });
 }
-// Переключить на следующую станцию
+// Метод GET для переключения на следующую станцию.
 app.get('/api/next-station', async (req, res) => {
     try {
         currentStationId++;
         const result = await pool.query('SELECT * FROM stations WHERE id = $1', [currentStationId]);
 
         if (result.rows.length === 0) {
-            // Если станция не найдена, возвращаемся к первой
+            // Если станция не найдена, возврат к первой.
             currentStationId = 1;
             const firstStation = await pool.query('SELECT * FROM stations WHERE id = $1', [currentStationId]);
             broadcast('station_changed', firstStation.rows[0]);
             res.json(firstStation.rows[0]);
         } else {
-            // Отправляем обновление всем клиентам
+            // Отправка обновления всем клиентам.
             broadcast('station_changed', result.rows[0]);
             res.json(result.rows[0]);
         }
@@ -77,10 +77,12 @@ app.get('/api/next-station', async (req, res) => {
     }
 });
 
+// Метод POST для установления режима.
 app.post('/api/game-mode', async (req, res) => {
     const { game_mode } = req.body;
     try {
-        broadcast('gamemode_changed', game_mode); // Отправляем сообщение WebSocket
+        // Отправляем широковещательное сообщение WebSocket.
+        broadcast('gamemode_changed', game_mode);
         res.status(200).send('Game mode changed');
     } catch (error) {
         console.error(error);
@@ -89,37 +91,40 @@ app.post('/api/game-mode', async (req, res) => {
 });
 
 
-// Настройка хранилища для загруженных файлов
+// Настройка хранилища для загруженных файлов.
 const storage = multer.diskStorage({
+    // Путь для сохранения файлов.
     destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // папка для сохранения файлов
+        cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
-        // Генерация безопасного имени файла
-        const ext = path.extname(file.originalname); // Получаем расширение
+        // Генерация безопасного имени файла.
+        // Получение расширения.
+        const ext = path.extname(file.originalname);
         const baseName = path.basename(file.originalname, ext);
-
-        // Удаляем пробелы и приводим к безопасному формату
+        // Удаление пробелов и приведение к безопасному формату.
         const safeName = baseName
-            .replace(/[^a-zA-Z0-9]/g, '_') // Заменяем все не буквенно-цифровые символы на "_"
+            // Замена всех не буквенно-цифровые символов на "_".
+            .replace(/[^a-zA-Z0-9]/g, '_') 
             .toLowerCase();
-
-        const uniqueSuffix = Date.now(); // Уникальный суффикс
-        cb(null, `${safeName}-${uniqueSuffix}${ext}`); // имя файла: название-дата.расширение
+        // Добавление уникального суффикса.
+        const uniqueSuffix = Date.now();
+        // Формат имени файла: название-дата.расширение.
+        cb(null, `${safeName}-${uniqueSuffix}${ext}`);
     },
 });
 
 const upload = multer({ storage });
 
-// Маршрут для создания станции
+// Метод POST для создания станции.
 app.post('/api/stations', upload.single('image'), async (req, res) => {
     const { name, description, stage } = req.body;
     const imagePath = req.file ? req.file.path : null;
 
-    // Создание нового объекта станции
+    // Создание нового объекта станции.
     const newStation = { name, description, stage, image: imagePath };
 
-    // Используем вашу таблицу stations для сохранения
+    // Сохранение в таблицу stations.
     const query = `
         INSERT INTO stations (name, description, stage, image)
         VALUES ($1, $2, $3, $4)
@@ -128,13 +133,13 @@ app.post('/api/stations', upload.single('image'), async (req, res) => {
     const values = [newStation.name, newStation.description, newStation.stage, newStation.image];
 
     try {
-        // Выполнение запроса к базе данных
+        // Выполнение запроса к базе данных.
         const result = await pool.query(query, values);
 
-        // Возвращаем успешный ответ с данными о новой станции
+        // Возвращение успешного ответа с данными о новой станции.
         res.status(201).send({
             message: 'Станция создана',
-            station: result.rows[0], // возвращаем данные из результата
+            station: result.rows[0],
         });
     } catch (error) {
         console.error('Error saving station to database:', error);
@@ -146,7 +151,7 @@ app.post('/api/stations', upload.single('image'), async (req, res) => {
 });
 
 
-// Получить все станции
+// Метод GET для получения списка всех станций.
 app.get('/api/stations', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM stations');
@@ -157,7 +162,7 @@ app.get('/api/stations', async (req, res) => {
     }
 });
 
-// Получить текущую станцию
+// Метод GET для получения текущей станции.
 app.get('/api/current-station', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM stations WHERE id = $1', [currentStationId]);
@@ -174,26 +179,25 @@ app.get('/api/current-station', async (req, res) => {
 
 
 
-
-// Оценить станцию и обновить средний рейтинг
+// Метод POST для оценки станции и обновления среднего рейтинга.
 app.post('/api/rating', async (req, res) => {
     const { participant_id, station_id, rating } = req.body;
 
     try {
-        // Добавляем новый рейтинг в таблицу ratings
+        // Добавляем новую оценку в таблицу ratings.
         await pool.query(
             'INSERT INTO ratings (participant_id, station_id, rating) VALUES ($1, $2, $3)',
             [participant_id, station_id, rating]
         );
 
-        // Пересчитываем средний рейтинг станции
+        // Пересчитывание среднего рейтинга станции с учётом новой оценки.
         const avgResult = await pool.query(
             'SELECT AVG(rating) as avg_rating FROM ratings WHERE station_id = $1',
             [station_id]
         );
         const avgRating = parseFloat(avgResult.rows[0].avg_rating).toFixed(2);
 
-        // Обновляем колонку rating в таблице stations
+        // Обновление столбца rating в таблице stations.
         await pool.query(
             'UPDATE stations SET rating = $1 WHERE id = $2',
             [avgRating, station_id]
@@ -208,7 +212,7 @@ app.post('/api/rating', async (req, res) => {
 
 
 
-// Получить всех участников
+// Метод GET для получения списка всех участников.
 app.get('/api/participants', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM participants');
@@ -220,7 +224,7 @@ app.get('/api/participants', async (req, res) => {
 });
 
 
-// Получить средний рейтинг станции
+// Метод GET для получения среднего рейтинга станций.
 app.get('/api/rating/:stationId', async (req, res) => {
     const { stationId } = req.params;
     try {
@@ -233,14 +237,12 @@ app.get('/api/rating/:stationId', async (req, res) => {
 });
 
 
-
-
-// Запуск сервера
+// Запуск сервера (порт 5000).
 const server = app.listen(5000, () => {
     console.log('Server running on port 5000');
 });
 
-// Подключаем WebSocket-сервер к HTTP-серверу
+// Подключение WebSocket-сервера к HTTP-серверу.
 server.on('upgrade', (request, socket, head) => {
     wss.handleUpgrade(request, socket, head, (ws) => {
         wss.emit('connection', ws, request);
